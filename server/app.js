9ecb9e1d1da37c16b8640fe0d048b0b8c7237955
thagoo <lohitgowda56@gmail.com>
2022-12-24 21:02:29 +0530

    after all it was just a syntax error

--- server/app.js
+++ server/app.js
@@ -1,37 +1,43 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const cors = require('cors');
+const express = require("express");
+const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
 const app = express();
 
-app.use(cors({
-  origin:'http://localhost:8000',
-  methods: ['GET', 'POST']
-}));
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  })
+);
 
 app.use(express.json());
 
-mongoose.connect('mongodb+srv://user:ketOuhLzoRMck2aa@user.kohciug.mongodb.net/?retryWrites=true&w=majority');
-mongoose.connection.on('connected', () => {
-  console.log('MongoDB is connected');
+mongoose.connect(
+  "mongodb+srv://thagoo:stud@cluster0.213u1iw.mongodb.net/Tailor?retryWrites=true&w=majority"
+);
+mongoose.connection.on("connected", () => {
+  console.log("MongoDB is connected");
 });
 
-mongoose.connection.on('disconnected', () => {
-  console.log('MongoDB is disconnected');
+mongoose.connection.on("disconnected", () => {
+  console.log("MongoDB is disconnected");
 });
 
-const User = mongoose.model('User', {
+const UserSchema = new mongoose.Schema({
   firstName: String,
   lastName: String,
   email: String,
   password: String,
   country: String,
   city: String,
-  province: String
+  province: String,
 });
 
-app.post('/register', async (req, res) => {
+const User = mongoose.model("User", UserSchema);
+
+app.post("/register", async (req, res) => {
   try {
     const hashedPassword = await bcrypt.hash(req.body.password, 10);
 
@@ -42,33 +48,35 @@ app.post('/register', async (req, res) => {
       password: hashedPassword,
       country: req.body.country,
       city: req.body.city,
-      province: req.body.province
+      province: req.body.province,
     });
 
     await user.save();
 
-    const token = jwt.sign({ userId: user._id }, 'mysecretkey');
+    const token = jwt.sign({ userId: user._id }, "mysecretkey");
 
     res.send({ token });
   } catch (error) {
     res.status(500).send(error);
   }
-  
 });
 
-app.post('/login', async (req, res) => {
+app.post("/login", async (req, res) => {
   try {
     const user = await User.findOne({ email: req.body.email });
     if (!user) {
-      return res.status(401).send('Email or password is incorrect');
+      return res.status(401).send("Email or password is incorrect");
     }
 
-    const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password);
+    const isPasswordCorrect = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
     if (!isPasswordCorrect) {
-      return res.status(401).send('Email or password is incorrect');
+      return res.status(401).send("Email or password is incorrect");
     }
 
-    const token = jwt.sign({ userId: user._id }, 'mysecretkey');
+    const token = jwt.sign({ userId: user._id }, "mysecretkey");
 
     res.send({ token });
   } catch (error) {
@@ -76,25 +84,24 @@ app.post('/login', async (req, res) => {
   }
 });
 
-app.get('/protected', (req, res) => {
+app.get("/protected", (req, res) => {
   try {
-    const token = req.headers['authorization'].split(' ')[1];
+    const token = req.headers["authorization"].split(" ")[1];
 
-    const decoded = jwt.verify(token, 'mysecretkey');
+    const decoded = jwt.verify(token, "mysecretkey");
 
     User.findById(decoded.userId, (error, user) => {
       if (error) {
         return res.status(500).send(error);
       }
       if (!user) {
-        return res.status(401).send('Unauthorized');
+        return res.status(401).send("Unauthorized");
       }
       res.send(user.fname);
     });
   } catch (error) {
-    res.status(401).send('Unauthorized');
+    res.status(401).send("Unauthorized");
   }
 });
 
-app.listen(8000, () => console.log('Server listening on port 8000'));
-
+app.listen(8000, () => console.log("Server listening on port 8000"));
